09f0d8f0dec7f1043f5902305223016cab5003ee
Nicole Rosa <nickrosa0901@gmail.com>
2026-05-12 21:30:40 -0300

    Delete questao1.js

--- questao1.js
+++
@@ -1,9 +0,0 @@
-const numeros = [10, 20, 30, 40, 50];
-let somaTotal = 0;
-
-for (let i = 0; i < numeros.length; i++) {
-  somaTotal = somaTotal + numeros[i];
-}
-
-let media = somaTotal / numeros.length;
-console.log("A média é: " + media);
