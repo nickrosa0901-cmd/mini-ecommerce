cba4ab6391c993fb36a7a7decaceb4cc8798b039
Nicole Rosa <nickrosa0901@gmail.com>
2026-05-12 21:33:04 -0300

    Delete questao5.js

--- questao5.js
+++
@@ -1,8 +0,0 @@
-const texto = "programação";
-let textoInvertido = "";
-
-for (let i = texto.length - 1; i >= 0; i--) {
-  textoInvertido = textoInvertido + texto[i];
-}
-
-console.log("Palavra invertida: " + textoInvertido);
